7bc453773a9bf5811bb713816a758924f0273297
Bartosz Błaszków <bartosz.blaszkow@swmansion.com>
2025-05-14 09:48:33 +0200

    Fix redefinition warn, cleanup code

--- lib/mix/tasks/compile.popcorn.ex
+++ lib/mix/tasks/compile.popcorn.ex
@@ -3,14 +3,15 @@ defmodule Mix.Tasks.Compile.Popcorn do
 
   @impl true
   def run(_args) do
-    # {opts, _, _} = OptionParser.parse(args, switches: [force: :boolean, compile_path: :string])
-    app_entrypoint = get_starting_point()
-    entrypoint = create_starting_point(app_entrypoint)
+    app_entrypoint = get_app_entrypoint()
+    {module, filename} = create_start_module(app_entrypoint)
 
-    Popcorn.pack(start_module: entrypoint)
+    Popcorn.pack(start_module: module)
+    # Remove generated module to not include it in regular application on next compilation
+    File.rm!(filename)
   end
 
-  defp get_starting_point() do
+  defp get_app_entrypoint() do
     config = Mix.Project.config()
     app = Keyword.get(config, :app)
 
@@ -19,31 +20,26 @@ defmodule Mix.Tasks.Compile.Popcorn do
         app_mod
 
       _ ->
-        raise "Missing application starting point. Please provide `:mod` in applocation config of your `mix.exs`"
+        raise "Missing application starting point. Please provide `:mod` in application config of your `mix.exs`"
     end
   end
 
   # module is expected to implement Application behaviour
-  defp create_starting_point({module, args}) do
+  defp create_start_module({module, args}) do
     contents =
       quote do
         def start() do
-          :erlang.display("Entrystart")
           unquote(module).start(:normal, unquote(args))
-          :ok
         end
       end
 
-    Mix.shell().info(Macro.to_string(contents))
     module_name = Popcorn.Entrypoint
 
-    # TODO: `mix compile.app` will autogenerate `:modules` key for app,
-    # resulting in this module being autoloaded and then trigger a warning about module redefinition
     {:module, _module_name, binary_content, _term} =
       Module.create(module_name, contents, Macro.Env.location(__ENV__))
 
     filename = Mix.Project.compile_path() |> Path.join("#{module_name}.beam")
     File.write!(filename, binary_content)
-    module_name
+    {module_name, filename}
   end
 end
